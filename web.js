d7d5c35abe27bddb4c67d8fe7b03209c09b37723
David Dailor <davedailor@comcast.net>
2013-07-11 04:41:39 +0000

    added two basic buffer declarations

--- web.js
+++ web.js
@@ -1,9 +1,7 @@
 var express = require('express');
 
-
-
-
-
+var buffer = new Buffer("Hello World 4 buffer","utf-8");
+var message = buffer.toString("utf-8");
 
 var app = express.createServer(express.logger());
 
