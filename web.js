4d01bfdf91f27d7b05e41dfdcee924dd2e6121f5
David Dailor <davedailor@comcast.net>
2013-07-11 04:55:28 +0000

    putting file read into message

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require ('fs');
-var msg2 = fs.readFileSync("index.html");
+var msg2 = new Buffer(fs.readFileSync("index.html"),"utf-8");
 
 var buffer = new Buffer("Hello World 4 buffer","utf-8");
 var message = buffer.toString("utf-8");
@@ -8,7 +8,7 @@ var message = buffer.toString("utf-8");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(message);
+  response.send(msg2.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
