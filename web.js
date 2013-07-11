07116b630cdaac58c6cd321535a67f135463d50a
David Dailor <davedailor@comcast.net>
2013-07-11 04:08:40 +0000

    used FileSync and buffer to get string in

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var infile = "index.html";
+var buffer = new Buffer(28);
+buffer = readFileSynce(infile);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
