7907fadea6d7ddbcc8a67b5b2e17ebe687fc65ab
David Dailor <davedailor@comcast.net>
2013-07-14 22:06:33 +0000

    added new bootstrap.css files and created index3.html

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require ('fs');
-var msg2 = new Buffer(fs.readFileSync("index.html"),"utf-8");
+var msg2 = new Buffer(fs.readFileSync("index3.html"),"utf-8");
 
 var buffer = new Buffer("Hello World 4 buffer","utf-8");
 var message = buffer.toString("utf-8");
