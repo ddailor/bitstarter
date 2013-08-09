53d7a9b0272425f3a762188c13411dee0044437e
David Dailor <davedailor@comcast.net>
2013-08-09 17:37:15 +0000

    Added savings calculator page

--- web.js
+++ web.js
@@ -10,6 +10,7 @@ var msg7 = new Buffer(fs.readFileSync("shop.html"),"utf-8");
 var msg8 = new Buffer(fs.readFileSync("control.html"),"utf-8");
 var msg9 = new Buffer(fs.readFileSync("setup.html"),"utf-8");
 var msgA = new Buffer(fs.readFileSync("schedule.html"),"utf-8");
+var msgB = new Buffer(fs.readFileSync("savings.html"), "utf-8");
  
 var buffer = new Buffer("Hello World 4 buffer","utf-8");
 var message = buffer.toString("utf-8");
@@ -45,6 +46,8 @@ app.get('/setup', function(request, response) {
 app.get('/schedule', function(request, response) {
     response.send(msgA.toString("utf-8"));
 });
+app.get('/savings', function(request,response) {
+    response.send(msgB.toString("utf-8"));
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
